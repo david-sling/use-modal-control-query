a83cef8f185b97b7134fbcf3428749e78a09a98f
David S D <david.sd@intelxlabs.com>
2025-08-01 12:11:21 +0530

    added the hook

---
+++ registry/hooks/use-modal-control-query.ts
@@ -0,0 +1,64 @@
+import { useRouter } from "next/navigation";
+import { Options, parseAsString, useQueryState } from "nuqs";
+
+export const useModalControlQuery = <HasState extends boolean = false>(
+  action_name: string,
+  {
+    openBehaviour = "push",
+    closeBehaviour = "back",
+    key = "modal",
+    onlyExplicitOpen,
+    hasState,
+  }: {
+    openBehaviour?: "push" | "replace";
+    closeBehaviour?: "push" | "replace" | "back";
+    key?: string;
+    onlyExplicitOpen?: boolean;
+    hasState?: HasState;
+  } = {}
+) => {
+  const [openSheet, setOpenSheet] = useQueryState(key, parseAsString);
+  const router = useRouter();
+
+  const [open_action, state] = hasState
+    ? (openSheet?.split(":") ?? [])
+    : [openSheet];
+
+  const set = (
+    open: boolean | (HasState extends true ? string : boolean),
+    options?: Options
+  ) => {
+    if (open)
+      setOpenSheet(
+        [action_name, ...(typeof open === "string" ? [open] : [])].join(":"),
+        {
+          history: openBehaviour,
+          shallow: true,
+          ...options,
+        }
+      );
+    else if (closeBehaviour === "back") router.back();
+    else
+      setOpenSheet(null, {
+        history: closeBehaviour,
+        shallow: true,
+        ...options,
+      });
+  };
+
+  return {
+    control: {
+      open: open_action === action_name,
+      onOpenChange: (open: boolean) => {
+        if (open && onlyExplicitOpen) return;
+        set(open);
+      },
+    },
+    state: (typeof state === "string"
+      ? state
+      : openSheet) as HasState extends true
+      ? string | boolean | null
+      : boolean | null,
+    set,
+  };
+};
