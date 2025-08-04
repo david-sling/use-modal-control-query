41a7f4d22a1a1448a5d2e5d77d46059fd14662d8
David S D <david.sd@intelxlabs.com>
2025-08-04 19:25:25 +0530

    update: registry.json

--- app/page.tsx
+++ app/page.tsx
@@ -22,15 +22,7 @@ export default function Home() {
           <h1 className="text-4xl font-bold tracking-tight">useModalControlQuery</h1>
         </div>
         <p className="text-muted-foreground">
-          A hook to control shadcn{' '}
-          <Link target="_blank" href="https://ui.shadcn.com/docs/components/sheet">
-            <code className="underline font-bold">sheet</code>
-          </Link>{' '}
-          and{' '}
-          <Link target="_blank" href="https://ui.shadcn.com/docs/components/dialog">
-            <code className="underline font-bold">dialog</code>
-          </Link>{' '}
-          components using query params
+          A hook to control shadcn modal components using query params
         </p>
       </header>
       <main className="flex flex-col flex-1 gap-16">
